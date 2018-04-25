bf4646e9c6c19e685f8f75e4ed92c91a75c16170
christine-aqui <2739361+christine-aqui@users.noreply.github.com>
2018-04-24 22:49:16 -0400

    almost got everyhting working, now to get win and lost text to display on screen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,45 +1,87 @@
 console.log('test connction');
 
 // var
-// var letter = ["a","b","c","d","e",
-// "f","g","h","i","j",
-// "k","l","m","n","o",
-// "p","q","r","s","t",
-// "u","v","w","x","y","z"]
+var letter = ["a", "b", "c", "d", "e",
+  "f", "g", "h", "i", "j",
+  "k", "l", "m", "n", "o",
+  "p", "q", "r", "s", "t",
+  "u", "v", "w", "x", "y", "z"
+]
 var maxNum = 3;
 var winCount = 0;
 var lostCount = 0;
-document.onkeyup = function (event){
-// get use input and makeit lower case
-var userGuess = event.key.toLowerCase();
-console.log(userGuess);
+var numLeft = 6 ;
 //
-var winText = "<p>Player Guess was: " + userGuess + "</p>";
-document.querySelector("#yourGuess").innerHTML = winText;
 //
-function letterChecker(userGuess) {
-  var letter = ["a","b","c","d","e",
-                "f","g","h","i","j",
-                "k","l","m","n","o",
-                "p","q","r","s","t",
-                "u","v","w","x","y","z"]
-                //
-var aiGuess = letter[Math.floor(Math.random() * letter.length)];
-console.log('ai guess ' + aiGuess);
 //
-  for (var i = 0; i < letter.length; i++) {
-    if (aiGuess === userGuess) {
-      console.log('you guessed correct ' + userGuess);
-      break
-    } else {
-      console.log('you guessed incorrect');
-    }
+//
+var winMessage = '<p> Player wins: ' + winCount +'</p>';
+document.querySelector("#wins").innerHTML = winMessage;
+//
+var loseMessage = '<p> Player loses: ' + lostCount +'</p>';
+document.querySelector("#loses").innerHTML = loseMessage;
+//
+var leftMessage = '<p> # of guesses left: ' + numLeft +'</p>';
+document.querySelector("#left").innerHTML = leftMessage;
+//
+// var leftMessage = '<p> # of guesses left: ' + left +'</p>';
+// document.querySelector("#left").innerHTML = leftMessage;
+//
+//
+//
+//
+document.onkeyup = function (event) {
+  // get use input and makeit lower case
+  var userGuess = event.key.toLowerCase();
+  //
+  //
+  // log it for testing
+  console.log(userGuess);
+  //
+  //
+  // html inline text create
+  var userText = "<h3>Player Guess was: " + userGuess + "</h3>";
+  document.querySelector("#yourGuess").innerHTML = userText;
+  //
+  //
+  // function that take in a letter array and randoms pick one
+  function computerGuess(letta) {
+    // console.log('letta ' + letta);
+    var aiGuess = letta[Math.floor(Math.random() * letta.length)];
+    console.log('ai guess ' + aiGuess);
+    return aiGuess;
   }
+  //
+  //
+  //
+  //
+  //
+  //
+  //Check the
+  function letterChecker(userGues) {
+    var randomletter = computerGuess(letter);
+    //
+    for (var i = 0; i < letter.length; i++) {
+      if (randomletter === userGues) {
+        winCount +=1;
+        console.log('Player win: ' + winCount);
+        console.log('you guessed correct ' + userGues);
+        return winCount;
+      } else {
+        lostCount += 1;
+        console.log('Player win: ' + lostCount);
+        //
+        var compText = "<h3>Player Guess was: " + randomletter + "</h3>";
+        document.querySelector("#compGuess").innerHTML = compText;
+        console.log('you guessed incorrect');
+        return lostCount;
+      }
+    }
+  } // end of letterChecker function
+  //
+  //
+  // call the function and pass in UserGuess
+  letterChecker(userGuess);
 }
 
-// call the function
-letterChecker(userGuess);
-}
 
-
-//
