8417e4eb17ca2e0069de4a906311045ebe413055
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-04-27 00:10:02 -0400

    redid most of it and took out jquery for now

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,74 +1,55 @@
-console.log('test connction');
 
-// var
-var letter = ["a", "b", "c", "d", "e",
-  "f", "g", "h", "i", "j",
-  "k", "l", "m", "n", "o",
-  "p", "q", "r", "s", "t",
-  "u", "v", "w", "x", "y", "z"
-]
-var maxNum = 3;
-var winCount = 0;
-var lostCount = 0;
-var numLeft = 6 ;
+var letter = ["a", "b", "c", "d",
+"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 //
+var winsDisplay = document.querySelector("#wins");
+var lossesDisplay = document.querySelector("#loses");
+var guessesLeftDisplay = document.querySelector("#left");
+var guessesMadeDisplay = document.querySelector("#yourGuess")
 //
-//                    Some DOM mods   ///
+var userWinCount = 0;
+var userLossCount = 0;
+var userGuessesLeft = 9;
+var guessesMade = [];
+var randomletter = letter[Math.floor(Math.random() * letter.length)];
+// var userGuess;
 //
-// H                  Hll of Functions //
-//
-// function that take in a letter array and randoms pick one
-function computerGuess(letta) {
-  // console.log('letta ' + letta);
-  var aiGuess = letta[Math.floor(Math.random() * letta.length)];
-  console.log('ai guess ' + aiGuess);
-  return aiGuess;
-} // end of computer guess function
-//
-function letterChecker(userGues) {
-  var randomletter = computerGuess(letter);
-
-  for (var i = 0; i < letter.length; i++) {
-    if (randomletter === userGues) {
-      winCount +=1;
-      console.log('Player win: ' + winCount);
-      console.log('you guessed correct ' + userGues);
-      return winCount;
-    } else {
-      lostCount += 1;
-      console.log('Player lose: ' + lostCount);
-      //
-      var compText = "<h3>Computer Guess was: " + randomletter + "</h3>";
-      document.querySelector("#compGuess").innerHTML = compText;
-      console.log('you guessed incorrect');
-      return lostCount;
-    }
+document.addEventListener('keyup', function (event) {
+  console.log('Player : ' + event.key);
+  console.log('Computer : ' + randomletter);
+  //If the user presses a key that is not A-Z, the program will do nothing.
+  if (letter.indexOf(event.key) > -1) {
+      guessCheck();
   }
-} // end of letterChecker function
-//
-document.onkeyup = function (event) {
-  // get use input and makeit lower case
-  var userGuess = event.key.toLowerCase();
-  //
-  // html inline text create
-  var userText = "<h3>Player Guess was: " + userGuess + "</h3>";
-  document.querySelector("#yourGuess").innerHTML = userText;
+});
 
-  // call the function and pass in UserGuess
-  letterChecker(userGuess);
-  var winMessage = '<p> Player wins: ' + winCount +'</p>';
-  // document.querySelector("#wins").innerHTML = winMessage;
-  $("#wins").html(winMessage);
-  //
-  var loseMessage = '<p> Player loses: ' + lostCount +'</p>';
-  // document.querySelector("#loses").innerHTML = loseMessage;
-  $("#loses").html(loseMessage);
-  //
-  var leftMessage = '<p> # of guesses left: ' + numLeft +'</p>';
-  // document.querySelector("#left").innerHTML = leftMessage;
-  $("#left").html(leftMessage);
+function guessCheck(){
+  if (event.key === randomletter) {
+      //When the player wins, increase the Wins counter,
+      userWinCount++;
+      winsDisplay.textContent = userWinCount;
+      //and start the game over again
+      reset();
+  } else {
+      guessesMade.push(event.key);
+      guessesMadeDisplay.textContent = guessesMade;
+      userGuessesLeft--;
+      guessesLeftDisplay.textContent = userGuessesLeft;
+      if (userGuessesLeft === 0) {
+          userLossCount++;
+          lossesDisplay.textContent = userLossCount;
+          reset();
+      }
+  }
 }
-
-
-// Notes to self.... function should always have a return...
-// dont forget that again.
+//
+function reset(){
+  //Generate a new random number.
+  randomletter = letter[Math.floor(Math.random() * letter.length)];
+  //When the game ends, guesses made should reset to null
+  guessesMade = [];
+  guessesMadeDisplay.textContent = guessesMade;
+  //When the game ends, guesses left should reset to 9
+  userGuessesLeft = 9;
+  guessesLeftDisplay.textContent = userGuessesLeft;
+}
