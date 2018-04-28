01d8de2a2cd59c5c3a9d1e5d1c8e9e75f10677bf
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-04-28 10:23:42 -0400

    added css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
-
 var letter = ["a", "b", "c", "d",
-"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
 //
 var winsDisplay = document.querySelector("#wins");
 var lossesDisplay = document.querySelector("#loses");
@@ -15,41 +15,41 @@ var randomletter = letter[Math.floor(Math.random() * letter.length)];
 // var userGuess;
 //
 document.addEventListener('keyup', function (event) {
-  console.log('Player : ' + event.key);
-  console.log('Computer : ' + randomletter);
-  //If the user presses a key that is not A-Z, the program will do nothing.
-  if (letter.indexOf(event.key) > -1) {
-      guessCheck();
-  }
+    console.log('Player : ' + event.key);
+    console.log('Computer : ' + randomletter);
+    //If the user presses a key that is not A-Z, the program will do nothing.
+    if (letter.indexOf(event.key) > -1) {
+        guessCheck();
+    }
 });
 
-function guessCheck(){
-  if (event.key === randomletter) {
-      //When the player wins, increase the Wins counter,
-      userWinCount++;
-      winsDisplay.textContent = userWinCount;
-      //and start the game over again
-      reset();
-  } else {
-      guessesMade.push(event.key);
-      guessesMadeDisplay.textContent = guessesMade;
-      userGuessesLeft--;
-      guessesLeftDisplay.textContent = userGuessesLeft;
-      if (userGuessesLeft === 0) {
-          userLossCount++;
-          lossesDisplay.textContent = userLossCount;
-          reset();
-      }
-  }
+function guessCheck() {
+    if (event.key === randomletter) {
+        //When the player wins, increase the Wins counter,
+        userWinCount++;
+        winsDisplay.textContent = userWinCount;
+        //and start the game over again
+        reset();
+    } else {
+        guessesMade.push(event.key);
+        guessesMadeDisplay.textContent = guessesMade;
+        userGuessesLeft--;
+        guessesLeftDisplay.textContent = userGuessesLeft;
+        if (userGuessesLeft === 0) {
+            userLossCount++;
+            lossesDisplay.textContent = userLossCount;
+            reset();
+        }
+    }
 }
 //
-function reset(){
-  //Generate a new random number.
-  randomletter = letter[Math.floor(Math.random() * letter.length)];
-  //When the game ends, guesses made should reset to null
-  guessesMade = [];
-  guessesMadeDisplay.textContent = guessesMade;
-  //When the game ends, guesses left should reset to 9
-  userGuessesLeft = 9;
-  guessesLeftDisplay.textContent = userGuessesLeft;
+function reset() {
+    //Generate a new random number.
+    randomletter = letter[Math.floor(Math.random() * letter.length)];
+    //When the game ends, guesses made should reset to null
+    guessesMade = [];
+    guessesMadeDisplay.textContent = guessesMade;
+    //When the game ends, guesses left should reset to 9
+    userGuessesLeft = 9;
+    guessesLeftDisplay.textContent = userGuessesLeft;
 }
